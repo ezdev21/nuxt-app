62127a0cc256d9e88c4567b99825812abdf5067e
gech11 <getachewfikadu77@gmail.com>
2021-11-24 00:29:58 +0300

    initialcommit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>{{x}}</p>
+  </div>
+</template>
+<script>
+export default {
+ head(){
+  return{
+   title:"about",
+   meta:[
+    {
+     hid:"about",
+     name:"about",
+     content:"about us" 
+    }
+   ]
+  }
+ },
+ data(){
+  return{
+   x:0
+  }
+ }
+}
+</script>
